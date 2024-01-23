492aee6bdf6e740c0353efebf2ee662360c8dcfd
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2024-01-23 18:32:03 +0200

    double jump

--- Assets/PROJECT/1. Scripts/Movement/ThirdPersonControllerNew.cs
+++ Assets/PROJECT/1. Scripts/Movement/ThirdPersonControllerNew.cs
@@ -4,58 +4,33 @@ using UnityEngine;
 
 public class ThirdPersonControllerNew : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-
     public float rotationSpeed = 5f;
     public float pitchRangeTop = 80f;
     public float pitchRangeBot = -80f;
     private float pitch = 0f;
 
-    public float jumpForce = 10f;
-    public int maxJumps = 2;
-
-    private bool isGrounded;
-    private int jumpsRemaining;
-    private float currentVelocityY;
-
-    private float Gravity = -15.0f;
-    private bool Grounded = true;
-    private float GroundedOffset = -0.14f;
-    private float GroundedRadius = 0.28f;
-    public LayerMask GroundLayers;
-    private float _verticalVelocity;
-    private float _jumpTimeoutDelta;
-    private float _fallTimeoutDelta;
-    private float _terminalVelocity = 53.0f;
-    private int jumpCount = 0; // Счетчик прыжков
-    public int maxJumpCount = 2; // Максимальное количество прыжков
-    public float JumpPower1 = 2f;
-    public float JumpPower2 = 2f;
-    public float FallTimeout = 0.15f;
-    public float JumpHeight = 1.2f;
-    public float JumpTimeout = 0.50f;// 0.50f;
-
-    [SerializeField] private Rigidbody rb;
+    public float jumpForce = 5;
+    public float groundDistance = 0.5f;
+    bool isGrounded = false;
     [SerializeField] private GameObject _mainCamera;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        // Убедитесь, что Rigidbody установлен как kinematic
-        rb.isKinematic = true;
-        jumpsRemaining = maxJumps;
-    }
+    [SerializeField] private Rigidbody rigidBody;
+    [SerializeField] private bool canDoubleJump;
+
+    public float GroundedRadius = 0.28f;
+    public float GroundedOffset = -0.14f;
+    public LayerMask GroundLayers;
+
 
     private void FixedUpdate()
     {
-        Rotate();
+    }
+
+    private void Update()
+    {
         GroundedCheck();
-        Move();
-        // Проверяем, нажата ли клавиша прыжка и есть ли еще оставшиеся прыжки
-        if (Input.GetButtonDown("Jump") && jumpsRemaining > 0)
-        {
-            Jump();
-        }
+        Rotate();
+        Jump();
     }
 
     private void Rotate()
@@ -74,80 +49,9 @@ public class ThirdPersonControllerNew : MonoBehaviour
         _mainCamera.transform.localRotation = Quaternion.Euler(pitch, 0f, 0f);
     }
 
-    private void Move()
+    private void CheckIsGrounded()
     {
-        // Получаем ввод от пользователя
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        // Вычисляем вектор направления
-        Vector3 movement = new Vector3(horizontal, 0f, vertical) * moveSpeed * Time.deltaTime;
-
-        // Применяем движение к Rigidbody
-        rb.MovePosition(rb.position + transform.TransformDirection(movement));
-
-        transform.Translate(Vector3.up * currentVelocityY * Time.deltaTime, Space.World);
-    }
-
-    private void JumpAndGravity()
-    {
-        if (Grounded)
-        {
-            jumpCount = 0;
-            //Debug.Log("JumpCount1 " + jumpCount);
-            _fallTimeoutDelta = FallTimeout;
-
-            if (_verticalVelocity < 0.0f)
-            {
-                _verticalVelocity = -JumpPower1;
-            }
-
-            if (_input.jump && _jumpTimeoutDelta <= 0.0f && jumpCount == 0)
-            {
-                _verticalVelocity = Mathf.Sqrt(JumpHeight * -JumpPower1 * Gravity);
-                jumpCount++;
-                //Debug.Log("JumpCount2 " + jumpCount);
-            }
-
-            if (_jumpTimeoutDelta >= 0.0f)
-            {
-                _jumpTimeoutDelta -= Time.deltaTime;
-            }
-
-            if (jumpCount == 0)
-            {
-                _input.jump = false;
-                _input.doubleJump = false;
-            }
-        }
-        else
-        {
-            if (jumpCount < maxJumpCount)
-            {
-                _jumpTimeoutDelta = JumpTimeout;
-                if (_input.doubleJump)
-                {
-                    //Debug.Log("doubleJump");
-                    //Debug.Log("_verticalVelocity " + _verticalVelocity);
-                    jumpCount++;
-                    // Debug.Log("JumpCount3 " + jumpCount);
-
-                    _verticalVelocity = Mathf.Sqrt(JumpHeight * -JumpPower2 * Gravity);
-                    _fallTimeoutDelta += FallTimeout;
-                }
-                if (_fallTimeoutDelta >= 0.0f)
-                {
-                    _fallTimeoutDelta -= Time.deltaTime;
-                }
-
-                //Debug.Log("JumpCount4 " + jumpCount);
-            }
-        }
-
-        if (_verticalVelocity < _terminalVelocity)
-        {
-            _verticalVelocity += Gravity * Time.deltaTime;
-        }
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundDistance);
     }
 
     private void GroundedCheck()
@@ -155,16 +59,38 @@ public class ThirdPersonControllerNew : MonoBehaviour
         // set sphere position, with offset
         Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset,
             transform.position.z);
-        Grounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers,
+        isGrounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers,
             QueryTriggerInteraction.Ignore);
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void Jump()
     {
-        // При соприкосновении с землей сбрасываем количество оставшихся прыжков
-        if (collision.gameObject.CompareTag("Ground"))
+        if (Input.GetKeyDown("space"))
         {
-            jumpsRemaining = maxJumps;
+            if (isGrounded)
+            {
+                rigidBody.velocity = Vector3.up * jumpForce;
+                canDoubleJump = true;
+            }
+            else if (canDoubleJump)
+            {
+                rigidBody.velocity = Vector3.up * jumpForce;
+                canDoubleJump = false;
+            }
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
+        Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
+
+        if (isGrounded) Gizmos.color = transparentGreen;
+        else Gizmos.color = transparentRed;
+
+        // when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
+        Gizmos.DrawSphere(
+            new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z),
+            GroundedRadius);
     }
 }
