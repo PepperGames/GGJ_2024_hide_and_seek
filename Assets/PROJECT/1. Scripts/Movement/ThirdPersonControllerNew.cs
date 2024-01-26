759f840c4934751c614328d4528148ac48bb7e83
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2024-01-26 15:35:36 +0200

    FixedUpdate() =>Update()

--- Assets/PROJECT/1. Scripts/Movement/ThirdPersonControllerNew.cs
+++ Assets/PROJECT/1. Scripts/Movement/ThirdPersonControllerNew.cs
@@ -72,7 +72,7 @@ public abstract class ThirdPersonControllerNew : MonoBehaviourPun
         }
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         if (photonView.IsMine)
         {
