66899caf2b4897570e26521b912b7137cc2c5d9e
Артем Кривошей <thedragonyt@gmail.com>
2024-01-31 20:13:32 +0200

    fixes

--- Assets/PROJECT/1. Scripts/Utilities/PlayerNicknameDisplay.cs
+++ Assets/PROJECT/1. Scripts/Utilities/PlayerNicknameDisplay.cs
@@ -4,38 +4,56 @@ using TMPro; // Подключаем для работы с TextMeshPro
 
 public class PlayerNicknameDisplay : MonoBehaviourPun
 {
-    public TMP_Text nicknameText; // Ссылка на TextMeshPro объект, который будет отображать никнейм
-    //public Vector3 offset = new Vector3(0, 2, 0); // Смещение текста относительно позиции игрока
-
+    public TMP_Text nicknameText; // Ссылка на TextMeshPro объект
     private Transform cameraTransform; // Для хранения ссылки на трансформ камеры
 
     private void Start()
     {
         cameraTransform = Camera.main.transform;
 
-        // Установка никнейма из Photon
+        // Установка никнейма из Photon и проверка на видимость
         if (photonView.IsMine)
         {
             nicknameText.text = PhotonNetwork.NickName;
+            SetNicknameVisibility(true); // Всегда видим свой никнейм
         }
         else
         {
             nicknameText.text = photonView.Owner.NickName;
+            CheckAndSetNicknameVisibility();
         }
-
-        // Синхронизация никнейма с другими клиентами
-        photonView.RPC("SyncNickname", RpcTarget.OthersBuffered, nicknameText.text);
     }
 
     private void Update()
     {
         // Поворачиваем никнейм всегда к камере
-        nicknameText.transform.rotation = Quaternion.LookRotation(nicknameText.transform.position - cameraTransform.position);
+        if (nicknameText.gameObject.activeSelf) // Обновляем только если никнейм активен
+        {
+            nicknameText.transform.rotation = Quaternion.LookRotation(nicknameText.transform.position - cameraTransform.position);
+        }
+    }
+
+    private void CheckAndSetNicknameVisibility()
+    {
+        // Получение команды локального и отображаемого игрока
+        string localPlayerTeam = PhotonNetwork.LocalPlayer.CustomProperties[ConstantsHolder.TEAM_PARAM_NAME]?.ToString();
+        string displayedPlayerTeam = photonView.Owner.CustomProperties[ConstantsHolder.TEAM_PARAM_NAME]?.ToString();
+
+        // Сравнение команд и установка видимости
+        bool isSameTeam = localPlayerTeam == displayedPlayerTeam;
+        SetNicknameVisibility(isSameTeam);
+    }
+
+    private void SetNicknameVisibility(bool isVisible)
+    {
+        nicknameText.gameObject.SetActive(isVisible);
     }
 
     [PunRPC]
     void SyncNickname(string nickname)
     {
         nicknameText.text = nickname;
+        CheckAndSetNicknameVisibility(); // Повторная проверка видимости после синхронизации
     }
 }
+
